142cacd77bbfcab6acd04bf49934dfa17eb7cba2
Fábio Júlio <73951977+fabiojuliosantos@users.noreply.github.com>
2024-01-15 15:41:56 -0300

    Conclusão da Minimal API inserindo os últimos métodos de update e delete. Atualização do README da aplicação.

--- ApiTarefas/ApiTarefas/Program.cs
+++ ApiTarefas/ApiTarefas/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 
+#region Configuração
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -17,22 +18,73 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+#endregion Configuração
 
+#region Gets
 app.MapGet("/", () => "Hello World");
 
-app.MapGet("/tarefas", async (AppDbContext db) =>  await db.Tarefas.ToListAsync());
+app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
+
+app.MapGet("/tarefas/{id}", async (int id, AppDbContext db) =>
+    await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound("Tarefa não encontrada!")
+);
+
+app.MapGet("/tarefas/concluidas", async (AppDbContext db) =>
+                                  await db.Tarefas.Where(t => t.Concluida).ToListAsync());
+#endregion Gets
+
+#region Post
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
+#endregion Post
+
+#region Put
+app.MapPut("/tarefas/{id}", async (int id, Tarefa inputTarefa, AppDbContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+
+    if (tarefa is null) return Results.NotFound("Erro: essa tarefa não existe!");
+
+    tarefa.Nome = inputTarefa.Nome;
+    tarefa.Concluida = inputTarefa.Concluida;
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+
+});
+#endregion Put
+
+#region Delete
+app.MapDelete("/tarefas/{id}", async (int id, AppDbContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+    if (tarefa is null) return Results.NotFound("Erro: essa tarefa não existe!");
+    db.Tarefas.Remove(tarefa);
+    await db.SaveChangesAsync();
+
+    return Results.Ok(tarefa);
+});
+#endregion Delete
 
 app.Run();
 
+#region Model Tarefa
 class Tarefa
 {
     public int Id { get; set; }
     public string? Nome { get; set; }
     public bool Concluida { get; set; }
 }
+#endregion Model Tarefa
 
+#region Context
 class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<Tarefa> Tarefas => Set<Tarefa>();
 }
+#endregion Context
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # :computer: ApiCatalogoMinimalApi
 ## :book: Repositório de uma Api de Tarefas
-`Repositório de Estudos sobre API utilizando .NET com a abordagem de Minimal Api`
+#### Repositório de Estudos sobre API utilizando .NET com a abordagem de Minimal Api
 
-`Minimal APIs utilizam uma quantidade menor de recursos se comparadas às APIs convencionais, sendo ideais para aplicações que consumam o mínimo de dependências do ASP.NET Core, por exemplo.`
+Minimal APIs utilizam uma quantidade menor de recursos se comparadas às APIs convencionais, sendo ideais para aplicações que consumam o mínimo de dependências do ASP.NET Core, por exemplo.
 
-`Todos os recursos necessários ficam presentes na classe Program.cs como classes das models, configurações e controllers`
+Todos os recursos necessários ficam presentes na classe Program.cs como classes das models, configurações e controllers
 
 #### Tecnologias utilizadas:
 - .Net 7;
@@ -16,3 +16,11 @@
 |Rota|Verbo Http|Funcionalidade|
 |:---:|:--:|:----:|
 |/tarefas|GET|Lista todas as Tarefas|
+|/tarefas{id}|GET|Mostra a tarefa do id informado|
+|/tarefas/concluidas|GET|Lista as tarefas concluídas|
+|/tarefas|POST|Insere uma nova tarefa|
+|/tarefas{id}|PUT|Atualiza a tarefa informada pelo id|
+|/tarefas{id}|DELETE|Exclui a tarefa informada pelo id|
+
+#### Observações:
+- Todos os dados são salvos em memória pelo Entity Framework, ou seja. A cada execução da aplicação, os dados precisam ser persistidos pelo método POST.
